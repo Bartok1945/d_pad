398a4ae512befa157c05c886bb5bdf2e342ff18f
rjkenney11 <rachelkenney@Rachels-MBP.fios-router.home>
2021-04-24 11:59:59 -0400

    added GidDisplay to homepage

--- client/src/components/Bar/Bar.js
+++ client/src/components/Bar/Bar.js
@@ -1,28 +1,36 @@
 import React from "react";
-import './Bar.css';
-
+import "./Bar.css";
 
 function Bar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-right">
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarText">
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
         <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-                <a className="nav-link" href="index.html">Swipe</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="portfolio.html">Playlist</a>
-            </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="index.html">
+              Swipe
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="portfolio.html">
+              Playlist
+            </a>
+          </li>
         </ul>
-        <span className="navbar-text">
-        </span>
-    </div>
+        <span className="navbar-text"></span>
+      </div>
     </nav>
-    
   );
 }
 
--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Bar from '../../components/Bar/Bar'
+import Bar from '../../components/Bar/Bar';
+import GifDisplay from '../../components/GifDisplay/GifDisplay';
 
 const Homepage = () => {
     return (
         <div>
             <Bar />
+            <GifDisplay />
             
         </div>
     )
