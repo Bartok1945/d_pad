3419a138fff97c6f1344c86fc262d460ecd1a73f
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-04-27 22:15:50 -0400

    added more to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './index.css'
-import Homepage from './pages/Homepage/Homepage';
+// import Homepage from './pages/Homepage/Homepage';
+// import GenresPages from './pages/GenresPage/GenresPage';
+import ConsolePages from './pages/ConsolePage/ConsolePage'
 
 function App() {
   return (
-    <Homepage />
+     // <Homepage />
+    // <GenresPages />
+    <ConsolePages />
   );
 }
 
