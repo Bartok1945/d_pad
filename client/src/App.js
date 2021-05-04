992ceff2cfb27ef7c3e9511187017eed01185e5e
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-05-04 18:53:45 -0400

    added in swipe

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import PageWrapper from './components/PageWrapper/PageWrapper';
 import Homepage from './pages/Homepage/Homepage';
 import ConsolePages from './pages/ConsolePage/ConsolePage';
+import Swipes from './pages/Swipes/Swipes';
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <Switch>
             <Route exact path={["/", "/Homepage"]} component={Homepage} />
             <Route exact path={"/ConsolePages"} component={ConsolePages} />
+            <Route exact path={"/Swipe"} component={Swipes}/>
           </Switch>
         </PageWrapper>
         
