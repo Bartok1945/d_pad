c8a72f45a2d0b5e7fa4e3005c6897b8b2443ce49
Nick Vandenberge <nvandenberge14@gmail.com>
2021-05-05 19:00:24 -0400

    added logout route

--- client/src/components/Bar/Bar.js
+++ client/src/components/Bar/Bar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Bar.css";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 function Bar() {
   return (
@@ -20,15 +19,20 @@ function Bar() {
       <div className="collapse navbar-collapse" id="navbarText">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-              <Link to = "/Swipes" className="nav-link">
-                Swipe
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to = "/playlist" className="nav-link">
-                Playlist
-              </Link>
-            </li>
+            <Link to="/Swipes" className="nav-link">
+              Swipe
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/playlist" className="nav-link">
+              Playlist
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Logout
+            </Link>
+          </li>
         </ul>
         <span className="navbar-text"></span>
       </div>
