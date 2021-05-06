f22c529b364919619ef5a6acbaca26d4d14cebe3
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-05-06 00:56:16 -0400

    fixed switch display

--- client/src/components/GameCard/GameCard.js
+++ client/src/components/GameCard/GameCard.js
@@ -7,25 +7,26 @@ function GameCard(props) {
       <div className="switch d-flex justify-self-center">
         <div className="controller">
           <div className="controller-left left col-l-2 col-m-2 col-s-2">
-            <div className="button-minus"></div>
-            <div
-              onClick={() => props.removeGame(props.id)}
-              className="remove"
-              id="controller-joystick"
-            >
-              <i className="fas fa-thumbs-down"></i>
-            </div>
-            <div className="button-top button-top-right"></div>
-            <div className="buttonsDivL justify-container-center ml-5">
-              <div className="button"></div>
-              <div className="button"></div>
-              <div className="button"></div>
-              <div className="button"></div>
-            </div>
-            <div className="button button-home"></div>
+              <div className="button-minus"></div>
+              <div
+                onClick={() => props.removeGame(props.id)}
+                className="remove"
+                id="controller-joystick"
+              >
+              <div className="downBtn p-3 m-2"><i className ="fas fa-thumbs-down fa-3x"/></div>
+              </div>
+                <div className="button-top button-top-right">
+                <div className="buttonsDivL justify-container-center m-5">
+                  <div className="button"></div>
+                  <div className="button"></div>
+                  <div className="button"></div>
+                  <div className="button"></div>
+                </div>
+                <button className="button-home"/>
+              </div>
           </div>
 
-          <div className="jumbotron gameInfo">
+          <div className="jumbotron gameInfo col-l-8 col-m-8 col-s-8">
             <div className="container-text">
               <div className="jumbotron-body">
                 <div className="card">
@@ -39,7 +40,7 @@ function GameCard(props) {
                   <div className="content">
                     <ul>
                       <li>
-                        <strong>Title:</strong> {props.name}
+                        <strong className="title">Title:</strong> {props.name}
                       </li>
                       <li>
                         <strong>Platform(s):</strong> {props.platforms}
@@ -59,26 +60,26 @@ function GameCard(props) {
               </div>
             </div>
           </div>
-          <div>
-            <div className="controller-right col-l-2 col-m-2 col-s-2">
-              <div className="button-plus"></div>
+      
+          <div className="controller-right col-l-2 col-m-2 col-s-2">
+            <div className="button-plus"></div>
               <div
                 onClick={() => props.addGame(props.id)}
                 className="add"
                 id="controller-joystick"
               >
-                <i className="fas fa-thumbs-up"></i>
-              </div>
-              <div className="button-top button-top-right"></div>
-              <div className="buttonsDivR justify-container-center ml-5">
-                <div className="button"></div>
-                <div className="button"></div>
-                <div className="button"></div>
-                <div className="button"></div>
-              </div>
-              <div className="button button-capture"></div>
+              <div className="upBtn"><i className ="fas fa-thumbs-up fa-3x"/></div>
             </div>
+            <div className="button-top button-top-right"></div>
+            <div className="buttonsDivR justify-container-center m-5">
+              <div className="button"></div>
+              <div className="button"></div>
+              <div className="button"></div>
+              <div className="button"></div>
+            </div>
+            <div className="button button-capture"></div>
           </div>
+        
         </div>
       </div>
     </div>
