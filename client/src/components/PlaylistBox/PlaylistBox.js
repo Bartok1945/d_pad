394d74b90ec09422657fc97ebe3e1d67d4bed6f5
Nick Vandenberge <nvandenberge14@gmail.com>
2021-05-07 14:25:24 -0400

    slight refactor

--- client/src/components/PlaylistBox/PlaylistBox.js
+++ client/src/components/PlaylistBox/PlaylistBox.js
@@ -1,77 +1,75 @@
 import React from "react";
-import "./PlaylistBox.css"
-import Container from 'react-bootstrap/Container'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import "./PlaylistBox.css";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 import { Row } from "react-bootstrap";
-import Col from 'react-bootstrap/Col'
+import Col from "react-bootstrap/Col";
 
-const games = [
-    {id:"Hollow Knight",played:false},
-    {id:"Borderlands",played:false},
-    {id:"Little Big Planet 2",played:false},
-    {id:"Crash Bandicoot",played:true},
-    {id:"Spyro",played:true},
-    {id:"Mario Party 2",played:true},
-    {id:"The Lion King",played:true},
+const PlaylistBox = () => {
+  const games = [
+    { id: "Hollow Knight", played: false },
+    { id: "Borderlands", played: false },
+    { id: "Little Big Planet 2", played: false },
+    { id: "Crash Bandicoot", played: true },
+    { id: "Spyro", played: true },
+    { id: "Mario Party 2", played: true },
+    { id: "The Lion King", played: true },
+  ];
 
-]
+  return (
+    <div>
+      <Container fluid>
+        <Row className="justify-content-md-center mainBox" md={1}>
+          <Jumbotron fluid>
+            <Row>
+              <Col md={3}>
+                <div className="logo">
+                  <img
+                    className="logo"
+                    src="https://user-images.githubusercontent.com/74163812/116794268-b77be380-aa99-11eb-9b5c-c3298c088690.png"
+                    alt="highscore"
+                  />
+                </div>
+              </Col>
 
-
-class PlaylistBox extends React.Component {
-
-
-    constructor(props){
-        super(props);
-        this.state = {
-            values: []
-        }
-    }
-
-    render () {
-        return (
-            <div>
-            <Container fluid>
-                <Row className="justify-content-md-center mainBox" md={1}>
-                    <Jumbotron fluid>
-                        <Row>
-                            <Col md={3}>
-                                <div className="logo">
-                                    <img className="logo"
-                                        src="https://user-images.githubusercontent.com/74163812/116794268-b77be380-aa99-11eb-9b5c-c3298c088690.png"
-                                        alt="highscore"/>
-                                </div>
-                            </Col>
-                        
-                            <Col md={{ span: 6, offset: 1}}> 
-                                <Row>
-                                    <Col md={{ span: 3, offset: 1}}>
-                                    <div className="container1">
-                                        <h2>Games to beat:</h2>
-                                        {games.map((game) => (
-                                        <ul>
-                                        <li><p>{game.id}</p><button className="played" id={game.id}>Played</button></li>
-                                        </ul>
-                                        ))};
-                                    </div>
-                                    </Col>
-                                    <Col md={{ span: 3, offset: 4}}>
-                                    <div className="container2">
-                                        <h2>Games in the Vault:</h2>
-                                        {games.map((game) => (
-                                        <ul>
-                                        <li>{game.id}</li><button className="delete" id={game.id}>Delete</button>
-                                        </ul>
-                                        ))};
-                                    </div>
-                                    </Col>
-                                </Row>
-                            </Col>
-                        </Row>
-                    </Jumbotron>
-                </Row> 
-            </Container>
-      </div>
-    );}
-} 
+              <Col md={{ span: 6, offset: 1 }}>
+                <Row>
+                  <Col md={{ span: 3, offset: 1 }}>
+                    <div className="container1">
+                      <h2>Games to beat:</h2>
+                      {games.map((game) => (
+                        <ul>
+                          <li>
+                            <p>{game.id}</p>
+                            <button className="played" id={game.id}>
+                              Played
+                            </button>
+                          </li>
+                        </ul>
+                      ))}
+                    </div>
+                  </Col>
+                  <Col md={{ span: 3, offset: 4 }}>
+                    <div className="container2">
+                      <h2>Games in the Vault:</h2>
+                      {games.map((game) => (
+                        <ul>
+                          <li>{game.id}</li>
+                          <button className="delete" id={game.id}>
+                            Delete
+                          </button>
+                        </ul>
+                      ))}
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Jumbotron>
+        </Row>
+      </Container>
+    </div>
+  )
+};
 
 export default PlaylistBox;
--- client/src/pages/PlaylistPage/PlaylistPage.js
+++ client/src/pages/PlaylistPage/PlaylistPage.js
@@ -3,35 +3,19 @@ import PageWrapper from "../../components/PageWrapper/PageWrapper";
 import Bar from "../../components/Bar/Bar";
 import PlaylistBox from "../../components/PlaylistBox/PlaylistBox";
 
-const games = [
-  {id:"Hollow Knight",played:false},
-  {id:"Borderlands",played:false},
-  {id:"Little Big Planet 2",played:false},
-  // {id:"Crash Bandicoot",played:true},
-  // {id:"Spyro",played:true},
-  // {id:"Mario Party 2",played:true},
-  // {id:"The Lion King",played:true},
+const PlaylistPage = () => {
+  const games = [
+    { id: "Hollow Knight", played: false },
+    { id: "Borderlands", played: false },
+    { id: "Little Big Planet 2", played: false },
+  ];
 
-]
-
-
-class PlaylistPage extends Component {
-  // Setting this.state.games to the games json array
-
-
-  render() {
-    return (
-
-      <PageWrapper>
-        <Bar/>
-        {games.map((games) => (
-        <PlaylistBox
-        id={games.id}
-        played={games.played}/>
-        ))}
-      </PageWrapper>
-    );
-  }
+  return (
+    <PageWrapper>
+      <Bar />
+      <PlaylistBox id={games.id} played={games.played} />
+    </PageWrapper>
+  );
 };
 
 export default PlaylistPage;
