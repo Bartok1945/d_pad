711441f44d7378e2bd0cca7ce6a5c633cadbc5a8
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-05-06 23:48:10 -0400

    tried to fix playlist

--- client/src/pages/PlaylistPage/PlaylistPage.js
+++ client/src/pages/PlaylistPage/PlaylistPage.js
@@ -3,15 +3,32 @@ import PageWrapper from "../../components/PageWrapper/PageWrapper";
 import Bar from "../../components/Bar/Bar";
 import PlaylistBox from "../../components/PlaylistBox/PlaylistBox";
 
+const games = [
+  {id:"Hollow Knight",played:false},
+  {id:"Borderlands",played:false},
+  {id:"Little Big Planet 2",played:false},
+  // {id:"Crash Bandicoot",played:true},
+  // {id:"Spyro",played:true},
+  // {id:"Mario Party 2",played:true},
+  // {id:"The Lion King",played:true},
+
+]
+
+
 class PlaylistPage extends Component {
   // Setting this.state.games to the games json array
+
 
   render() {
     return (
 
       <PageWrapper>
         <Bar/>
-        <PlaylistBox/>
+        {games.map((games) => (
+        <PlaylistBox
+        id={games.id}
+        played={games.played}/>
+        ))}
       </PageWrapper>
     );
   }
